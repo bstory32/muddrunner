8be9bec8c40d5a97f196e6acdf08edf90ec0ebd3
Neil Sammons <neil.a.sammons@gmail.com>
2014-08-13 22:05:54 -0500

    modified projects section of the homepage

--- index.php
+++ index.php
@@ -1,146 +1,148 @@
 <?php include ('layout/header.php') ?>
- 
 
 
-    <!-- start of events section -->
 
-    <div class="background1">
-      <div class="container">
-        <div class="row">
+<!-- start of events section -->
 
-          <div class="col-sm-6 event-section">
-            <div class="event-header">
-              <h2>UPCOMING EVENTS</h2>
-            </div>
-            <ul class="event-list">
-              <li>
-                <img src="http://placehold.it/140x100" alt="">
-                <span>August 9 &amp 10, 2014</span>
-                <p>Portland</p>
-                <a href="#">95% Sold Out - Register >></a>
-                
-              </li>
-              <li>
-                <img src="http://placehold.it/140x100">
-                <span>August 16 &amp 17, 2014</span>
-                <p>Pittsburgh/Ohio Valley</p>
-                <a href="#">95% Sold Out - Register >></a>
-              </li>
-              <li>
-                <img src="http://placehold.it/140x100">
-                <span>August 16 &amp 17, 2014</span>
-                <p>Tahoe</p>
-                <a href="">Register >></a>
-              </li>
-              <li>
-                <img src="http://placehold.it/140x100">
-                <span>August 16 &amp 17, 2014</span>
-                <p>Toronto</p>
-                <a href="#">Register >></a>
-              </li>
-            </ul>
-            <div class="more">
-              <a href="#">View all Events >></a>
-            </div>
-          </div>
+<div class="background1">
+  <div class="container">
+    <div class="row">
 
-          <div class="col-sm-6 main-message">
-            <h1>I survived muddrunner.</h1>
-            <hr>
-            <img src="public/images/splatter.png" alt="">
-            <p>Neil Sammons</p>
-          </div>
+      <div class="col-sm-6 event-section">
+        <div class="event-header">
+          <h2>UPCOMING EVENTS</h2>
+        </div>
+        <ul class="event-list">
+          <li>
+            <img src="http://placehold.it/140x100" alt="">
+            <span>August 9 &amp 10, 2014</span>
+            <p>Portland</p>
+            <a href="#">95% Sold Out - Register >></a>
+
+          </li>
+          <li>
+            <img src="http://placehold.it/140x100">
+            <span>August 16 &amp 17, 2014</span>
+            <p>Pittsburgh/Ohio Valley</p>
+            <a href="#">95% Sold Out - Register >></a>
+          </li>
+          <li>
+            <img src="http://placehold.it/140x100">
+            <span>August 16 &amp 17, 2014</span>
+            <p>Tahoe</p>
+            <a href="">Register >></a>
+          </li>
+          <li>
+            <img src="http://placehold.it/140x100">
+            <span>August 16 &amp 17, 2014</span>
+            <p>Toronto</p>
+            <a href="#">Register >></a>
+          </li>
+        </ul>
+        <div class="more">
+          <a href="#">View all Events >></a>
+        </div>
+      </div>
+
+      <div class="col-sm-6 main-message">
+        <h1>I survived muddrunner.</h1>
+        <hr>
+        <!-- <img src="public/images/splatter.png" alt=""> -->
+        <p>Neil Sammons</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end of events section -->
+
+<!-- start of featured obstacle -->
+
+<div class="background-image">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 obstacle-section">
+        <div class="obstacle-info">
+          <h1>Muddy Expedition</h1>
+        </div>
+        <div class="obstacle-details">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet inventore vitae, architecto exercitationem. Tenetur necessitatibus repellat dolorum alias magnam aliquid nisi molestias, et id rem ducimus ea sed porro deleniti!</p>
+          <a href="#">See All >></a>
         </div>
       </div>
     </div>
-    <!-- end of events section -->
+  </div>
+</div>
 
-    <!-- start of featured obstacle -->
+<!-- end of featured of obstacle -->
 
-    <div class="background-image">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 obstacle-section">
-            <div class="obstacle-info">
-              <h1>Muddy Expedition</h1>
-            </div>
-            <div class="obstacle-details">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet inventore vitae, architecto exercitationem. Tenetur necessitatibus repellat dolorum alias magnam aliquid nisi molestias, et id rem ducimus ea sed porro deleniti!</p>
-              <a href="#">See All >></a>
-            </div>
-          </div>
+<!-- start of news section -->
+
+<div class="background2">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 news-section">
+        <div class="news-header">
+          <h2>News</h2>
+        </div>
+        <ul class="articles">
+          <li>
+            <img src="http://placehold.it/140x100">
+            <p>Meet the Muddrunner Founder</p>
+            <a href="#">Read Article >></a>
+          </li>
+
+          <li>
+            <img src="http://placehold.it/140x100">
+            <p>Are you tough enough?</p>
+            <a href="#">Read Article >></a>
+          </li>
+
+          <li>
+            <img src="http://placehold.it/140x100">
+            <p>Tank Away: Mudder Tough</p>
+            <a href="#">Read Article >></a>
+          </li>
+        </ul>
+
+        <div class="more">
+          <a href="#">See all news >></a>
+        </div>
+      </div>
+
+      <!-- end of new section -->
+
+      <!-- start of projects section -->
+
+      <div class="col-sm-6 more-info">
+        <div class="projects">
+          <h3>Mudd Project</h3>
+          <hr class="blue">
+          <img src="http://placehold.it/140x100">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quia quisquam enim atque totam cupiditate quaerat ducimus provident deserunt, hic reiciendis adipisci suscipit aperiam.</p>
+
+        </div>
+        <div class="more-projects">
+          <a href="#">Learn More >></a>
+        </div>
+        <div class="projects">
+          <h3>Train Tough</h3>
+          <hr class="blue">
+          <img src="http://placehold.it/140x100">
+          <p>Lorem ipsum dolor commodi, dolorum repellat maxime culpa fugit sunt omnis exercitationem id possimus dolore at ab porro.</p>
+
+        </div>
+        <div class="more-projects">
+          <a href="#">Shop More >></a>
         </div>
       </div>
     </div>
+  </div>
+</div>
 
-    <!-- end of featured of obstacle -->
-
-    <!-- start of news section -->
-
-    <div class="background2">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-6 news-section">
-            <div class="news-header">
-              <h2>News</h2>
-            </div>
-            <ul class="articles">
-              <li>
-                <img src="http://placehold.it/140x100">
-                <p>Meet the Muddrunner Founder</p>
-                <a href="#">Read Article >></a>
-              </li>
-              
-              <li>
-                <img src="http://placehold.it/140x100">
-                <p>Are you tough enough?</p>
-                <a href="#">Read Article >></a>
-              </li>
-              
-              <li>
-                <img src="http://placehold.it/140x100">
-                <p>Tank Away: Mudder Tough</p>
-                <a href="#">Read Article >></a>
-              </li>
-            </ul>
-            
-            <div class="more">
-              <a href="#">See all news >></a>
-            </div>
-          </div>
-
-          <!-- end of new section -->
-
-          <!-- start of projects section -->
-          
-          <div class="col-sm-6 more-info">
-            <div class="projects">
-              <h3>Mudd Project</h3>
-              <hr class="blue">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quia quisquam enim atque totam cupiditate quaerat ducimus provident deserunt, hic reiciendis adipisci suscipit aperiam.</p>
-              <img src="http://placehold.it/140x100">
-            </div>
-            <div class="more-projects">
-              <a href="#">Learn More >></a>
-            </div>
-            <div class="projects">
-              <h3>Train Tough</h3>
-              <hr class="blue">
-              <p>Lorem ipsum dolor commodi, dolorum repellat maxime culpa fugit sunt omnis exercitationem id possimus dolore at ab porro.</p>
-              <img src="http://placehold.it/140x100">
-            </div>
-            <div class="more-projects">
-              <a href="#">Shop More >></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- end of projects section -->
+<!-- end of projects section -->
 
 <?php include ('layout/footer.php') ?>
 
 
-  </body>
-  </html>
+</body>
+</html>
